d7d9754bc0019f3820ee716e661cbebfb2061a1e
ruserxd <zxcmomo0219@gmail.com>
2023-10-26 00:05:47 +0800

    my setBackGround have some trouble close finish this project

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,50 +1,192 @@
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.*;
 import java.util.Random;
+
 public class GamePanel extends JPanel implements ActionListener {
-    static  final  int  SCREEN_WIDTH  = 600;
-    static  final  int  SCREEN_HEIGHT = 600;
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
     //遊戲的像素大小
-    static  final  int  UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 50;
     //遊戲的面積
-    static  final  int  GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
-    static  final  int  DELAY  =  75;
+    static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+    static final int DELAY = 50;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
     GamePanel() {
-
+        random = new Random();
+        //這個物件的大小
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        //可以利用鍵盤輸入
+        this.setFocusable(true);
+        //將鍵盤事件監聽器添加到元件中,MyKeyAdapter作為觀察器
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
-    public  void startGame() {
 
+    public void startGame() {
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
-    public  void paintComponent(Graphics g) {
 
+    public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+        draw(g);
     }
-    public  void draw(Graphics g) {
 
-    }
-    public  void  move() {
+    public void draw(Graphics g) {
+        if(running) {
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                //畫直線
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                //畫橫線
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+            g.setColor(Color.red);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+            for (int i = 0; i < bodyParts; i++) {
+                //fillRect填滿一個矩形
+                if (i == 0) {
+                    g.setColor(Color.green);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+        } else {
 
+            gameOver(g);
+        }
     }
-    public  void checkApple() {
 
+    public void newApple() {
+        //隨機產生一個在範圍內的數字
+        appleX = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
     }
-    public  void checkCollisions() {
 
+    public void move() {
+        for (int i = bodyParts; i > 0; i--) {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch (direction) {
+        case 'U':
+            y[0] = y[0] - UNIT_SIZE;
+            break;
+        case 'D':
+            y[0] = y[0] + UNIT_SIZE;
+            break;
+        case 'L':
+            x[0] = x[0] - UNIT_SIZE;
+            break;
+        case 'R':
+            x[0] = x[0] + UNIT_SIZE;
+            break;
+        }
     }
-    public  void gameOver (Graphics g) {
 
+    public void checkApple() {
+        if((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
     }
+
+    //檢查碰撞
+    public void checkCollisions() {
+        //檢查頭部是否與身體碰撞
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i]) && (y[0] == y[i])) {
+                running = false;
+            }
+        }
+        //檢查是否撞到左邊的牆壁
+        if (x[0] < 0) {
+            running = false;
+        }
+        //檢查是否撞到右邊的牆壁
+        if (x[0] > SCREEN_WIDTH) {
+            running = false;
+        }
+        //檢查是否撞到頂部的牆壁
+        if (y[0] < 0) {
+            running = false;
+        }
+        //檢查是否撞到底部的牆壁
+        if (y[0] > SCREEN_HEIGHT) {
+            running = false;
+        }
+        if (!running) {
+            timer.stop();
+        }
+    }
+
+    public void gameOver(Graphics g) {
+        //遊戲結束畫面
+        g.setColor(Color.red);
+        g.setFont(new Font("INK FREE",Font.BOLD,75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("Game Over",(SCREEN_WIDTH - metrics.stringWidth("Gave Over"))/2,SCREEN_HEIGHT/2);
+    }
+
+    //@Override
     public void actionPerformed(ActionEvent e) {
-
+        if (running) {
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
-    public  class MykeyAdapter extends KeyAdapter {
-        @override
-        public  void keyPressed(KeyEvent e) {
 
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+            case KeyEvent.VK_LEFT:
+                if (direction != 'R') {
+                    direction = 'L';
+                }
+                break;
+            case KeyEvent.VK_RIGHT:
+                if (direction != 'L') {
+                    direction = 'R';
+                }
+                break;
+            case KeyEvent.VK_UP:
+                if (direction != 'D') {
+                    direction = 'U';
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+                if (direction != 'U') {
+                    direction = 'D';
+                }
+                break;
+            }
         }
     }
 }
